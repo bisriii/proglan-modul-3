fcc22273806a747b67c6af0b29464c8014f179aa
Bisri <bisrifarizi10@gmail.com>
2024-10-24 07:16:46 +0700

    first init github

---
+++ DependencyPractice/src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+ import org.apache.commons.lang3.StringUtils;
+
+ public class App {
+ public static void main(String[] args) {
+ String text = "Hello, Maven!";
+ System.out.println(text);
+  }
+ }
